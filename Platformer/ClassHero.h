57e8ae5e652ae201e0e823a82fe0970c35b5a0bd
Battary <49065223+MrBattary@users.noreply.github.com>
2019-04-09 18:11:16 +0300

    Patch 0.1.2
    0.1.2
    Добавлена боевая стойка(для переключения:клавиша Z)
    Боевая стойка:
    -Все передвижение и его анимации
    -Анимация afk
    Теперь для скольжения нужно удерживать клавишу С
    Исправлены незначительные баги и ошибки

---
+++ Platformer/ClassHero.h
@@ -0,0 +1,212 @@
+﻿#pragma once
+/*
+Тут описываем Героя, Его положение по х/y, анимации итд
+Только объявление! Исключение: Функции-Помощники
+*/
+
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class Hero
+{
+protected:
+	float xH;							//Положение виртуальной модели по х
+	float yH;							//Положение виртуальной модели по y
+	float wH;							//Высота модели
+	float hH;							//Ширина модели
+	float dxH;							//Ускорение по х
+	float dyH;							//Ускорение по у
+	float speedHX=0;					//Скорость персонажа по X
+	float speedHY=0;					//Скорость персонажа по Y
+	int dirH;							//Направление движения	Данная "роза направлений" будет использоваться часто,советую её запомнить.
+
+											//7 4 8
+											// ↖↑↗
+											//3←0→1
+											// ↙↓↘
+											//6 2 5
+
+	int healthH;						//Здоровье Героя 0-100
+
+	String File;						//Файл с расширением
+	Image image;						//Изображение
+	Texture texture;					//Текстура
+	Sprite sprite;						//Спрайт
+
+	bool battleMode = false;			//Боевая стойка
+	bool battleModeAvaible = false;		//Проверка на боевую стойку
+	bool moveLeft = true;				//Нужна для проверки на стандартное движение влево
+	bool moveOnlyLeft = true;			//Проверка на лево и прямо
+	bool moveRight = true;				//Нужна для проверки на стандартное движение вправо
+	bool moveOnlyRight = true;			//Проверка на право и прямо
+	bool buttonIsPressed = false;		//Нажата ли клавиша нужно для idle
+	bool previousDirectionMove = false; //Предыдущее направление движения false-вправо true-влево
+	bool crouchAviable = true;			//Можно ли присесть,исключает возможность воспроизведения анимации HeroCrouchIdle в движении 
+	bool slideAviable = false;			//Скользим?
+	bool jumpAviable = false;			//Прыгаем?
+	bool jumpLargeAviable = false;		//Далеко прыгаем?
+	bool controlAviable = true;			//Контроль управления с клавиатуры
+	bool onlyOneAnimation = true;		//Воспроизведение только одной анимации по всем направлениям
+	bool onlyOneAnimationOneDir = true;	//Воспроизведение только одной анимации в одном направлении
+
+	float currentFrame = 0;				//Номер текущего кадра циклической анимации
+	float currentUncycleFrame = 0;		//Номер текущего кадра не циклической анимации
+
+	float cooldownAnimationJump = 0;	//Кулдаун на анимацию прыжков
+	float cooldownAnimationSlide = 0;	//Кулдаун на анимацию скольжения
+public:
+	Hero(String F, float X, float Y, float W, float H, int Health)		//Конструктор 
+	{
+		File = F;
+		wH = W;
+		hH = H;
+		image.loadFromFile("Sprites/"+File);				//Загружаем в него файл
+		image.createMaskFromColor(Color(255, 255, 255));	//Прозрачность
+		texture.loadFromImage(image);						//Текстура
+		sprite.setTexture(texture);							//Спрайт
+		xH = X;
+		yH = Y;
+		healthH = Health;									//Здоровье
+		sprite.setTextureRect(IntRect(0, 0, wH, hH));		//Вырезаем персонажа
+	}
+	//Функции-CHANGE помощники могут изменять значения
+	void Change_speedHX(float value) { speedHX += value; }
+	void Change_speedHY(float value) { speedHY += value; }
+	void Change_cooldownAnimationJump(float value) { cooldownAnimationJump += value; }
+	void Change_cooldownAnimationSlide(float value) { cooldownAnimationSlide += value; }
+
+	//Функции-GET помощники
+	Sprite Get_Sprite() { return sprite; }										//Получить Спрайт
+	float Get_speedXH() { return speedHX; }										//speedHX
+	float Get_speedXY() { return speedHY; }										//speedHY
+	float Get_cooldownAnimationJump() { return cooldownAnimationJump; }			//cooldownAnimationJump
+	float Get_cooldownAnimationSlide() { return cooldownAnimationSlide; }		//cooldownAnimationSlide
+	bool Get_moveLeft() { return moveLeft; }									//Можно ли двигаться влево
+	bool Get_moveOnlyLeft() { return moveOnlyLeft; }							//Можно ли двигаться только прямо влево
+	bool Get_moveRight() { return moveRight; }									//Можно ли двигаться вправо
+	bool Get_moveOnlyRight() { return moveOnlyRight; }							//Можно ли двигаться только прямо вправо
+	bool Get_buttonIsPressed() { return buttonIsPressed; }						//Нажата ли кнопка
+	bool Get_previousDirectionMove() { return previousDirectionMove; }			//Узнать предыдущее направление false-вправо true-влево
+	bool Get_crouchAviable() { return crouchAviable; }							//Можно ли присесть true-да, false-нет
+	bool Get_slideAviable() { return slideAviable; }							//Доступно ли скольжение
+	bool Get_jumpAviable() { return jumpAviable; }								//Доступен ли прыжок
+	bool Get_jumpLargeAviable() { return jumpLargeAviable; }					//Доступен ли мощный прыжок
+	bool Get_controlAviable() { return controlAviable; }						//Доступно ли управление с клавиатуры
+	bool Get_onlyOneAnimation() { return onlyOneAnimation; }					//Возможно ли воспроизведение только одной анимации
+	bool Get_onlyOneAnimationOneDir() { return onlyOneAnimationOneDir; }		//Возможно ли воспроизведение только одной анимации в одном направлении
+	bool Get_battleMode() { return battleMode; }								//Какая стойка
+	bool Get_battleModeAvaible() { return battleModeAvaible; }					//Какая стойка возможна
+
+
+	//Функции-SET помощники
+	void Set_moveLeft(bool value) { moveLeft=value; }							//Установить движение влево
+	void Set_moveOnlyLeft(bool value) { moveOnlyLeft=value; }					//Установить движение только прямо влево
+	void Set_moveRight(bool value) { moveRight=value; }							//Установить движение вправо
+	void Set_moveOnlyRight(bool value) { moveOnlyRight=value; }					//Установить движение только прямо вправо
+	void Set_buttonIsPressed(bool value) { buttonIsPressed=value; }				//Установить нажатие клавиши
+	void Set_previousDirectionMove(bool value) { previousDirectionMove=value; }	//Установить предыдущее направление движения
+	void Set_crouchAviable(bool value) { crouchAviable=value; }					//Установить возможность приседать
+	void Set_slideAviable(bool value) { slideAviable=value; }					//Установить возможность скользить
+	void Set_jumpAviable(bool value) { jumpAviable=value; }						//Установить возможность прыжка
+	void Set_jumpLargeAviable(bool value) { jumpLargeAviable=value; }			//Установить возможность мощного прыжка
+	void Set_controlAviable(bool value) { controlAviable=value; }				//Установить возможность управления персонажем с клавиатуры
+	void Set_onlyOneAnimation(bool value) { onlyOneAnimation = value; }			//Установить воспроизведение только одной анимации
+	void Set_onlyOneAnimationOneDir(bool value){onlyOneAnimationOneDir = value;}//Установить воспроизведение только одной анимации в одном направлении
+	void Set_battleMode(bool value) { battleMode = value; }						//Установить стойку
+	void Set_battleModeAvaible(bool value) { battleModeAvaible = value; }		//Установить возможность смены стойки
+
+
+	void Draw(float);															//Отрисовка персонажа, связь физики и анимации
+
+	/*
+	Функции отвечающие за связь анимации и её физической составляющей
+	*/
+	//Движение вверх-вниз 
+	int HeroMoveTop(float,bool,bool,bool);
+	int HeroMoveDown(float,bool,bool,bool);
+
+	//AFK	
+	int HeroIdleLeft(float);
+	int HeroIdleRight(float);
+	int HeroIdleCrouchLeft(float);
+	int HeroIdleCrouchRight(float);
+
+	//Скольжение	
+	void HeroSlideChoise(float);
+	int HeroSlideRight(float);
+	int HeroSlideRightUp(float);
+	int HeroSlideRightDown(float);
+	int HeroSlideLeft(float);
+	int HeroSlideLeftUp(float);
+	int HeroSlideLeftDown(float);
+
+	//Выбор прыжка
+	void HeroJumpChoise(float);
+	void HeroJumpLargeChoise(float);
+
+	//Малые прыжки	
+	int HeroJumpRight(float);
+	int HeroJumpRightUp(float);
+	int HeroJumpRightDown(float);
+	int HeroJumpLeft(float);
+	int HeroJumpLeftUp(float);
+	int HeroJumpLeftDown(float);
+
+	//Большие прыжки	
+	int HeroJumpLargeRight(float);	
+	int HeroJumpLargeRightUp(float);
+	int HeroJumpLargeRightDown(float);
+	int HeroJumpLargeLeft(float);
+	int HeroJumpLargeLeftUp(float);
+	int HeroJumpLargeLeftDown(float);
+
+	//Движение вправо	
+	int HeroMoveRight(float);
+	int HeroMoveRightFast(float);
+	int HeroMoveRightSlow(float);
+	int HeroMoveRightCrouch(float);
+	int HeroMoveRightUp(float);
+	int HeroMoveRightFastUp(float);
+	int HeroMoveRightSlowUp(float);
+	int HeroMoveRightCrouchUp(float);
+	int HeroMoveRightDown(float);
+	int HeroMoveRightFastDown(float);
+	int HeroMoveRightSlowDown(float);
+	int HeroMoveRightCrouchDown(float);
+
+	//Движение влево	
+	int HeroMoveLeft(float);
+	int HeroMoveLeftFast(float);
+	int HeroMoveLeftSlow(float);
+	int HeroMoveLeftCrouch(float);
+	int HeroMoveLeftUp(float);
+	int HeroMoveLeftFastUp(float);
+	int HeroMoveLeftSlowUp(float);
+	int HeroMoveLeftCrouchUp(float);
+	int HeroMoveLeftDown(float);
+	int HeroMoveLeftFastDown(float);
+	int HeroMoveLeftSlowDown(float);
+	int HeroMoveLeftCrouchDown(float);
+	
+	//Боевка
+	int HeroChangeModToBattleLeft(float);
+	int HeroChangeModToCivilLeft(float);
+	int HeroChangeModToBattleRight(float);
+	int HeroChangeModToCivilRight(float);
+
+
+	//Через эту функцию происходит получение урона героем
+	void HeroGetDamage();
+	
+	//Диалоги героя
+	void HeroDialogs();
+
+
+	//~Hero() {}; Деструктор пока в комментах ибо нефиг
+};
+
+
+inline void Hero::HeroDialogs()
+{
+}
---
+++ Platformer/OptimizedFunctionsCHero.h
@@ -0,0 +1,143 @@
+﻿//Вспомогательные функции для функций персонажа
+/*
+Функция Physics: физика, отвечает за правильную физику в игре
+speedX					-ускорение по оси Х
+speedY					-ускорение по оси Y
+msX(movementspeedX=ms)	-значение по Х, к которому стремится speedX
+msY						-значение по Y, к которому стремится speedY
+clutchX/clutchY			-показатель сцепления с грунтом, чем он меньше, тем сильнее "заносит" персонажа, больше- обратная ситуация
+						 поможет сильно, когда будет ландшафт и разница в грунтах.
+addX/addY				-доп ускорение (для прыжков,рывков, итд), если не нужно:ставим 0.
+*/
+
+void Physics(float &speedX,float &speedY,float msX,float msY,float clutchX,float clutchY,float addX, float addY) {
+	speedX += addX;
+	speedY += addY;
+	
+	if (speedX > msX)speedX -= 0.02* clutchX;
+	if (speedX < msX)speedX += 0.02* clutchX;
+	if (speedX < msX + 0.02 && speedX > msX-0.02) speedX = msX;
+	
+	if (speedY > msY)speedY -= 0.02* clutchY;
+	if (speedY < msY)speedY += 0.02* clutchY;
+	if (speedY < msY + 0.02 && speedY > msY - 0.02) speedY = msY;
+}
+
+/*
+Функция CycleAnimation: воспроизведение ЦИКЛИЧЕСКОЙ анимации, именно она отвечает за двигающуюся картинку.
+currentFrame	-текущий кадр
+time			-часы
+animationSpeed	-скорость анимации
+frames			-количество кадров в анимации
+sprite			-спрайт
+setTextureRect	-вырезает квадратик 1 кадра из всего спрайта по координатам
+x1				-положение верхней точки по Х							х1
+y1				-положение верхней точки по Y						 у1 •――――――•
+x2				-положение нижней точки по Х							|      |
+y2				-положение нижней точки по Y							•――――――• y2
+x11				-нужен для отражения										   x2
+*/
+void CycleAnimation(float & currentFrame, float time, double animationSpeed, int frames,Sprite & sprite, int x1,int x11, int y1, int x2, int y2) {
+	currentFrame += animationSpeed * time;
+	if (currentFrame > frames) currentFrame -= frames;
+	sprite.setTextureRect(IntRect(x1 * int(currentFrame)+x11, y1, x2, y2));
+}
+
+/*
+Функция воспроизведения анимации скольжения,сначала воспроизводится нециклическая анимация, затем циклическая, затем снова не циклическая анимация
+cooldown		-куладун скольжения увеличивается на значение cooldownValue
+direction		-направление скольжения
+directionValue	-значение направления Смотри "розу направлений"
+currentFrameUN	-текущий кадр
+time			-часы
+animationSpeed	-скорость анимации
+frames			-количество кадров в анимации
+sprite			-спрайт
+y2				-положение нижней точки по Y	
+x11				-нужен для отражения
+aviable			-возвращает значение, выполняется ли еще эта анимация
+*/
+
+void OnlySlideAnimation(float & currentFrameUN, float time, float& cooldown, float cooldownValue, bool & avaible, int & direction, int directionValue, float & speedHX, float & speedHY, Sprite & sprite, int x11, int x2, float msX, float msY, float clutchX, float clutchY, float addX, float addY, double fHX, double sHX, double fHY, double sHY) {
+	cooldown += cooldownValue*time;
+	direction = directionValue;
+	currentFrameUN += 0.007 * time;
+	if (currentFrameUN <= 2.01) {
+		sprite.setTextureRect(IntRect(100 * int(currentFrameUN)+ x11, 592, x2, 74));
+	}
+	if (currentFrameUN <= 4.01 && currentFrameUN > 2 && (speedHX > fHX && speedHX< sHX && speedHY> fHY && speedHY < sHY)) {
+		if (Keyboard::isKeyPressed(Keyboard::C)) {
+			Physics(speedHX, speedHY, msX, msY, clutchX, clutchY, addX, addY);
+			if (currentFrameUN > 4) currentFrameUN -= 2;
+			sprite.setTextureRect(IntRect(100 * int(currentFrameUN) + x11, 592, x2, 74));
+		}
+		else currentFrameUN = 4.1;
+	}
+	if (currentFrameUN > 4) {
+		sprite.setTextureRect(IntRect(100 * int(currentFrameUN)+ x11, 592, x2, 74));
+	}
+	if (currentFrameUN > 8)
+	{
+		avaible = false;
+		currentFrameUN = 0;
+	}
+}
+
+/*
+Функция воспроизведения анимации прыжка, полностью не циклическая анимация
+cooldown		-куладун прыжка увеличивается на значение cooldownValue
+direction		-направление прыжка
+directionValue	-значение направления Смотри "розу направлений"
+currentFrameUN	-текущий кадр
+time			-часы
+animationSpeed	-скорость анимации
+frames			-количество кадров в анимации
+sprite			-спрайт
+y1				-положение верхней точки по Y
+y2				-положение нижней точки по Y
+x11				-нужен для отражения
+aviable			-возвращает значение, выполняется ли еще эта анимация
+fHX,sHX,fHY,SHY -ограничения скорости
+msX,msY,clutchX,clutchY,addX,addY - нужны для движка физики
+*/
+
+void OnlyJumpsAnimation(float & currentFrameUN, float time, float& cooldown, float cooldownValue, bool & avaible, int & direction, int directionValue, double animationSpeed, float & speedHX, float & speedHY, Sprite & sprite, int x11, int y1,int x2,int frames, float msX, float msY, float clutchX, float clutchY, float addX, float addY,double fHX, double sHX, double fHY, double sHY) {
+	cooldown += cooldownValue*time;
+	direction = directionValue;
+	currentFrameUN += animationSpeed * time;
+	if (speedHX > fHX && speedHX< sHX && speedHY> fHY && speedHY < sHY) {
+		Physics(speedHX, speedHY, msX, msY, clutchX, clutchY, addX, addY);
+	}
+	sprite.setTextureRect(IntRect(100 * int(currentFrameUN)+x11, y1, x2, 74));
+	if (currentFrameUN > frames)
+	{
+		avaible = false;
+		currentFrameUN = 0;
+	}
+}
+
+/*
+Функция UncycleAnimation : воспроизведение НЕЦИКЛИЧЕСКОЙ анимации.
+currentFrame	- текущий кадр
+time			- часы
+animationSpeed	- скорость анимации
+frames			- количество кадров в анимации
+sprite			- спрайт
+setTextureRect	- вырезает квадратик 1 кадра из всего спрайта по координатам
+x1				- положение верхней точки по Х							х1
+y1				- положение верхней точки по Y						 у1 •――――――•
+x2				- положение нижней точки по Х							|      |
+y2				- положение нижней точки по Y							•――――――• y2
+x11				- нужен для отражения										   x2
+avaible			- возвращает значение, выполняется ли еще эта анимация
+*/
+void UncycleAnimation(float & currentFrameUN, float time, bool & avaible, int & direction, int directionValue, double animationSpeed, Sprite & sprite, int x11, int y1, int x2, int frames) {
+	direction = directionValue;
+	currentFrameUN += animationSpeed * time;
+	sprite.setTextureRect(IntRect(100 * int(currentFrameUN) + x11, y1, x2, 74));
+	if (currentFrameUN > frames)
+	{
+		avaible = false;
+		currentFrameUN = 0;
+	}
+}
