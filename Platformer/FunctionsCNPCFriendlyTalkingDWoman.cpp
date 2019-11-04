3f095dac1979df672003fe2e47b07335d020f85b
MrBattary <49065223+MrBattary@users.noreply.github.com>
2019-11-04 20:45:29 +0600

    NPCFriendlyTalkingWoman takes advansed logic

--- Platformer/FunctionsCNPCFriendlyTalkingDWoman.cpp
+++ Platformer/FunctionsCNPCFriendlyTalkingDWoman.cpp
@@ -5,33 +5,27 @@
 
 void NPCFriendlyTalkingWoman::Logic(float time)
 {
-	switch (currentAction)
+	if (currentActionTime > 0) {								//Пока есть время на выполнение анимации
+		currentActionTime -= time;								//Время на поспроизведение анимации уменьшается
+	}
+	else														//Если время вышло
+	{
+		currentAction = RandomBetweenInt(1, 2);					//Определяется, какое действие будет выполняться
+		currentActionTime = RandomBetweenDouble(5, 10);			//Определяется время, в течении которого действие будет выполняться.
+		currentActionTime = currentActionTime / logicReaction;	//А деление на logicReaction необходимо, чтобы потом не пришлось каждый раз currentActionTime -= time * logicReaction;
+	}
+
+	switch (currentAction)										//Анимация выполняется в соответствии со своим номером
 	{
 	case 1:
-		if (currentActionTime > 0) {				//Пока есть время на выполнение анимации
-			WomanIdleRight(time);					//Анимация выполняется
-			currentActionTime -= time;				//Время на поспроизведение анимации уменьшается
-		}
-		else										//Если время вышло
-		{
-			currentActionTime = 0;					//Номер текущего действия сбрасывается в 0 (default)
-			currentAction = 0;						//Время на текущее действие сбрасывается в 0 (default)
-		}
+		WomanIdleRight(time);					
 		break;
-	case 2:
-		if (currentActionTime > 0) {			
-			WomanIdleLeft(time);					
-			currentActionTime -= time;				
-		}
-		else										
-		{
-			currentActionTime = 0;				
-			currentAction = 0;						
-		}
+	case 2:		
+		WomanIdleLeft(time);					
 		break;
 	default:
-		currentAction = RandomBetweenInt(1, 2);		//Определяется, какое действие будет выполняться
-		currentActionTime = RandomBetweenDouble(5 / logicReaction, 10 / logicReaction);	//Определяется время, в течении которого действие будет выполняться. А деление на logicReaction необходимо, чтобы потом не пришлось каждый раз currentActionTime -= time*logicReaction;
+		currentActionTime = 0;									//Номер текущего действия сбрасывается в 0 (default)
+		currentAction = 0;										//Время на текущее действие сбрасывается в 0 (default)
 		break;
 	}
 }
